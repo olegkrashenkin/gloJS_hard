6a05898c28cc9f554f864a26567f5a46324fbb0c
Oleg Krashenkin <k125629@ya.ru>
2022-12-05 19:54:19 +0300

    Решил первую задачу 3го урока

---
+++ script.js
@@ -0,0 +1,26 @@
+'use strict'
+
+// 1). Переменная lang может принимать 2 значения: 'ru' 'en'.
+// Написать условия при котором в зависимости от значения lang
+// будут выводится дни недели на русском или английском языке.
+let lang = 'ru'
+const daysOfWeek = {
+    ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+    en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+}
+// a) через if
+lang === 'ru'
+    ? console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье')
+    : console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday')
+
+// b) через switch-case
+switch (lang) {
+    case 'ru':
+        console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье')
+        break
+    case 'en':
+        console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday')
+}
+
+// c) через многомерный массив без ифов и switch.
+console.log(daysOfWeek[lang])
