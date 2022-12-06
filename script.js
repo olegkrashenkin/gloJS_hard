f17c51772667e3554d45c7ae907f4f33126d7a59
Oleg Krashenkin <k125629@ya.ru>
2022-12-06 19:08:54 +0300

    мелкие правки

--- script.js
+++ script.js
@@ -2,13 +2,19 @@
 
 const foo = function (str) {
     if (typeof str === 'string') {
-        if (str.trim().length > 30) return `${str.substr(0, 30)}...`
+        str = str.trim()
+        if (str.length > 30) {
+            return `${str.substr(0, 30)}...`
+        } else {
+            return str
+        }
     }
     return `${str} - не строка`
 }
 
 
-console.log(foo('1234567890123456789012345678901234567890'))
+console.log(foo('     1234567890123456789012345678901234567890  '))
+console.log(foo('  1234567890  '))
 console.log(foo(123))
 
 
