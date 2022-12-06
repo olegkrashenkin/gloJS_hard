eb79e8180df9e66f5c166de37c4cb95440c87d2b
Oleg Krashenkin <k125629@ya.ru>
2022-12-06 18:57:44 +0300

    "сложное" ДЗ к 4 уроку=)

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const foo = function (str) {
+    if (typeof str === 'string') {
+        str = str.trim()
+        if (str.length > 30) return `${str.substr(0, 30)}...`
+    }
+    return `${str} - не строка`
+}
+
+
+console.log(foo('1234567890123456789012345678901234567890'))
+console.log(foo(123))
+
+
