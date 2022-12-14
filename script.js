d7e1e695f02ad8dc1d49f010dc9923805063b187
olegkrashenkin <86842418+olegkrashenkin@users.noreply.github.com>
2022-12-14 18:01:17 +0300

    Done

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
 'use strict'
 
-const body = document.body
-const dayOfWeek = new Date().getDay()
-const weekDays = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-]
-
-weekDays.forEach((el, index) => {
-    let newEl = document.createElement('p')
-    switch (index) {
-        case dayOfWeek:
-            newEl.style.fontWeight = 700
-            break;
-        case 0:
-            newEl.style.fontStyle = 'italic'
-            break;
-        case 6:
-            newEl.style.fontStyle = 'italic'
-            break;
+let tikTok = () => {
+    const weekDays = ['Воскресение', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
+    const months = ['января', 'февраля', 'марта', 'апреля', 'мае', 'июня',
+        'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря']
+    let count = 1
+    const start = () => {
+        console.log(count);
+        count++
+        let date = new Date()
+        const tmp = document.querySelector('.wrap')
+        tmp.firstElementChild.textContent =
+            `Сегодня ${weekDays[date.getDay()]}, 
+        ${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()} года, 
+        ${date.getHours()} ${hour(date.getHours())} ${date.getMinutes()} минут ${date.getSeconds()} секунды`
+        tmp.lastElementChild.textContent =
+            `${zeros(date.getDate())}.${zeros(date.getMonth() + 1)}.${date.getFullYear()} - 
+        ${zeros(date.getHours())}:${zeros(date.getMinutes())}:${zeros(date.getSeconds())}`
     }
-    newEl.textContent = el
-    body.appendChild(newEl)
-})
+    const hour = (hour) => {
+        let tmp = String(hour).slice(-1)
+        switch (true) {
+            case hour >= 5 && hour <= 20 || tmp == 0: return 'часов'
+            case tmp == 1: return 'час'
+            default: return 'часа'
+        }
+    }
+    const zeros = (num) => {
+        num = String(num)
+        if (num.length != '2') return `0${num}`
+        return num
+    }
+    start()
+    setInterval(start, 1000)
+}
+
+tikTok()
+
+
