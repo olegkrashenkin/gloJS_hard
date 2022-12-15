a5897ae48a06e0b5ddcc40658670822b221d1a6e
olegkrashenkin <86842418+olegkrashenkin@users.noreply.github.com>
2022-12-15 04:41:22 +0300

    Очередные правки

--- script.js
+++ script.js
@@ -1,33 +1,53 @@
 'use strict'
 
 let tikTok = () => {
-    const weekDays = ['Воскресение', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
-    const months = ['января', 'февраля', 'марта', 'апреля', 'мае', 'июня',
-        'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря']
     const start = () => {
-        let date = new Date()
         const tmp = document.querySelector('.wrap')
+        const date = new Date()
+        const weekday = date.toLocaleString('ru', { weekday: 'long' })
+        const year = date.toLocaleString('ru', { year: 'numeric' })
+        const dayMonth = date.toLocaleString('ru', { day: 'numeric', month: 'long' })
+        const hour = date.toLocaleString('ru', { hour: 'numeric' })
+        const minute = date.toLocaleString('ru', { minute: 'numeric' })
+        const second = date.toLocaleString('ru', { second: 'numeric' })
+        const fullDate = date.toLocaleString('ru', { day: '2-digit', month: '2-digit', year: 'numeric' })
+        const fullTime = date.toLocaleString('ru', { hour: '2-digit', minute: '2-digit', second: 'numeric' })
+
         tmp.firstElementChild.textContent =
-            `Сегодня ${weekDays[date.getDay()]}, 
-        ${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()} года, 
-        ${date.getHours()} ${hour(date.getHours())} ${date.getMinutes()} минут ${date.getSeconds()} секунды`
-        tmp.lastElementChild.textContent =
-            `${zeros(date.getDate())}.${zeros(date.getMonth() + 1)}.${date.getFullYear()} - 
-        ${zeros(date.getHours())}:${zeros(date.getMinutes())}:${zeros(date.getSeconds())}`
+            `Сегодня ${weekday.charAt(0).toUpperCase() + weekday.slice(1)}, ${dayMonth} ${year} года, 
+        ${hour} ${decl(hour, 'hour')} ${minute} ${decl(minute, 'minute')} 
+        ${second} ${decl(second, 'second')}`
+
+        tmp.lastElementChild.textContent = `${fullDate} - ${fullTime}`
     }
-    const hour = (hour) => {
-        let tmp = String(hour).slice(-1)
-        switch (true) {
-            case hour >= 5 && hour <= 20 || tmp == 0: return 'часов'
-            case tmp == 1: return 'час'
-            default: return 'часа'
+
+    const decl = (units, flag = 'hour') => {
+        let tmp = String(units).slice(-1)
+        switch (flag) {
+            case 'hour': {
+                switch (true) {
+                    case units >= 5 && units <= 20 || tmp == 0: return 'часов'
+                    case tmp == 1: return 'час'
+                    default: return 'часа'
+                }
+            }
+            case 'minute': {
+                switch (true) {
+                    case units >= 5 && units <= 20 || tmp == 0 || tmp >= 5 && tmp <= 9: return 'минут'
+                    case tmp == 1: return 'минута'
+                    default: return 'минуты'
+                }
+            }
+            case 'second': {
+                switch (true) {
+                    case units >= 5 && units <= 20 || tmp == 0 || tmp >= 5 && tmp <= 9: return 'секунд'
+                    case tmp == 1: return 'секунда'
+                    default: return 'секунды'
+                }
+            }
         }
     }
-    const zeros = (num) => {
-        num = String(num)
-        if (num.length != '2') return `0${num}`
-        return num
-    }
+
     start()
     setInterval(start, 1000)
 }
